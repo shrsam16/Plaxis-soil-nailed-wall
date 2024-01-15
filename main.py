8bb1e9cb178eaf3577918c53f61789fe98914c19
Samyak <shrsam16@gmail.com>
2024-01-15 07:03:52 +0545

    models for E = 5000

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def getParameter(MODELNUMBER):
                 if p<30:
                     dilatency = 0
                 else:
-                    dilatency = p-30
+                    dilatency = 0
                 for c in C:#3
                     for n in Neu:#5
                         for ExDep in ExcavationDepth:#6
@@ -42,15 +42,15 @@ def getParameter(MODELNUMBER):
                                                 yield parameter
 
 parameterKeys = ['S.N.','E','Gam','phi','C','Neu','dil','ExDep','Bfill','Plthk','FAng','Inc','Sp','Len']
-E = [8000, 6500 ,5000]
-GammaUnsat = [19.5, 21.0, 22.5]
-phi = [26.0,30.0,34.0]
-C = [25.0,15.0,5.0]
-Neu = [0.35, 0.30]
+E = [5000] #[8000, 6500 ,5000]
+GammaUnsat =[22.5] #[19.5, 21.0, 22.5]
+phi = [26.0]#[26.0,30.0,34.0]
+C = [5.0]#[25.0,15.0,5.0]
+Neu = [0.30]
 
 #Geometry
-ExcavationDepth = [6.0]#[2.0,4.0,6.0,8.0,10.0]
-# BackfillAngle = [13.0]#[0.0, 13.0, 26.0] 
+ExcavationDepth = [2.0,4.0,6.0,8.0,10.0]
+#BackfillAngle = [13.0]#[0.0, 13.0, 26.0]
 FaceAngle = [5.0, 10.0]#[10.0, 5.0, 0.0]
 #Nail
 NailInclination = [15]#[13.0,15.0,18.0]
@@ -58,7 +58,7 @@ NailSpacing = [0.5, 0.7, 0.9]
 NailLength = [1.2, 1.0, 0.8]#[0.8, 0.7, 0.6]
 NailDiameter = 32.0
 #plate
-PlateThickness = [100, 200]#[150]
+PlateThickness = [150]#[100, 200]#
 
 if __name__ == "__main__":
     
